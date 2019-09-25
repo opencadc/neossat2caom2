83cf18fe6621cf74cc0e87023e4a8926f8266321
Sharon Goliath <goliaths.cadc@gmail.com>
2019-09-25 12:47:18 -0700

    s2535 - script to start the neossat container.

--- scripts/neossat_run.sh
+++ scripts/neossat_run.sh
@@ -6,7 +6,7 @@ cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
 echo "Get the container"
 # docker pull bucket.canfar.net/neossat2caom2 || exit $?
 COLLECTION="neossat"
-CONTAINER="${COLLECITON}_run_int"
+CONTAINER="${COLLECTION}_run_int"
 
 echo "Run container ${CONTAINER}"
 docker run --rm --name ${COLLECTION}_run -v ${PWD}:/usr/src/app/ ${CONTAINER} ${COLLECTION}_run || exit $?
